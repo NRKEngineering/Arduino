1c31876f445c51d87679fe0b638cb3df6524d1d6
NRKEngineering <nkent@tpg.com.au>
2018-07-03 14:10:20 +1000

    Add files via upload
    Added buzzer to alarm, also fixed some syntax issues

--- hallEffectSensor.ino
+++ hallEffectSensor.ino
@@ -1,23 +1,36 @@
 /*
-Arduino Hall Effect Sensor
-This program alows reading the output of a 3144E 
-hall effect sensor (Switch type sensor)
+HALL EFFECT SENSOR ALARM - ARDUINO
 
-Arduino type - Nano
+DESCRIPTION:
+	This program alows reading the output of a 3144E 
+	hall effect sensor (Switch type sensor) which switches
+	on and off an led and buzzer.
+
+AUTHOR:
+	Nicholas Kent
+
+DATE:
+	July 2018
+	
+ARDUINO TYPE:
+	Nano
 
 */
 
 // Constants
-hallIn = 2;		// Set input pin
-ledOut = 13;	// Led output
+const int hallIn = 2;			// Hall Effect input pin
+const int ledOut = 13;			// Led output
+const int buzzerOut = 9;		// Buzzer output
 
 // Variables
-hallState = 0;	// Stores current state of sensor
+int hallState = 0;				// Stores current state of sensor
+int buzzTone = 1000;			// Buzzer tone in Hz
 
 void setup() 
 {	// Set the pin modes
 	pinMode(hallIn, INPUT);		// Hall sensor input
 	pinMode(ledOut, OUTPUT);	// led output
+	pintMode(buzzerOut, OUTPUT);// Buzzer output
 }
 
 
@@ -27,12 +40,18 @@ void loop()
 	hallState = digitalRead(hallIn);
 	
 	if(hallIn == LOW)
-	{	// Turn on LED
+	{	// Turn on LED and buzzer
 		digitalWrite(ledPin, HIGH);
-		
+		tone(buzzerOut, buzzTone);
+		// Buzz for one second
+		delay(1000);
+		// Turn off buzzer for one second
+		noTone(buzzerOut);
+		delay(1000);
 	}
 	else
-	{	// Turn off LED
+	{	// Turn off LED and buzzer
 		digtialWrite(ledPin, LOW);
+		noTone(buzzerOut);
 	}
 }
